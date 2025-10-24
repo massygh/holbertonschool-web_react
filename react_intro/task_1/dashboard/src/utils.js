3c8c0ec050492537011befdc5358002bf59243fc
massygh <massyghen1@gmail.com>
2025-10-24 13:25:52 +0200

    edit readme

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,8 +1,17 @@
 // src/utils.js
+
+/**
+ * Returns the current year as a number
+ */
 export function getCurrentYear() {
   return new Date().getFullYear();
 }
 
+/**
+ * Returns the footer text depending on the isIndex boolean
+ * @param {boolean} isIndex
+ * @returns {string}
+ */
 export function getFooterCopy(isIndex) {
   return isIndex ? "Holberton School" : "Holberton School main dashboard";
 }
