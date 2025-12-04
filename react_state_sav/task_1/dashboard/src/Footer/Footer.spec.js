5d2734532b1dd9f34ebc029c65a1e31a4181d84b
massygh <massyghen1@gmail.com>
2025-12-04 14:32:05 +0100

    task 5

--- react_state_sav/task_1/dashboard/src/Footer/Footer.spec.js
+++
@@ -1,7 +0,0 @@
-import { getFooterCopy } from '../utils/utils';
-
-describe('Footer Component', () => {
-    test('Renders correct copyright string when getFooterCopy returns true', () => {
-        expect(getFooterCopy(true)).toBe('Holberton School');
-    });
-})
